b3c38ad71015465e163265656849c351d7a97cc9
Joel <joel.salminen@gmail.com>
2019-12-22 13:26:04 +0200

    Client added.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('hello');
+import express from 'express';
+import { port } from './config.js';
+
+const app = express();
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index', { answer: 59 });
+});
+
+app.listen(port, () => {
+  console.info(`Running on ${port}...`);
+});
